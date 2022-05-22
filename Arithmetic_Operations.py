2aa0194f2595af4e4319def8ea3662b83eeb67d0
Anilkumar Para <50998167+AnilkumarPara@users.noreply.github.com>
2022-05-22 12:31:30 +0530

    Add files via upload

---
+++ Arithmetic_Operations.py
@@ -0,0 +1,20 @@
+print("---I am in Arithmetic_Operations---")
+def addition(a,b):
+    print("---I am doing the addition---")
+    return a+b
+
+def subtraction(a,b):
+    print("---I am doing the subtraction---")
+    return a-b
+
+def multiplication(a,b):
+    print("---I am doing the multiplication---")
+    return a*b
+
+def division(a,b):
+    print("---I am doing the division---")
+    if b==0:
+        return 'cannot divide by zero'
+    else:
+        return a/b
+

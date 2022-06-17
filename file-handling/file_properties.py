4be4f0fac96606b2283c860d99d55b52d1e515f5
Anilkumar Para <50998167+AnilkumarPara@users.noreply.github.com>
2022-06-17 22:21:49 +0530

    Add files via upload

---
+++ file-handling/file_properties.py
@@ -0,0 +1,6 @@
+with open('Sample.txt',mode='r') as f:
+    print('name of the file=',f.name)
+    print('file number=',f.fileno)
+    print('mode of the file =',f.mode)
+    print('file is closed or not =',f.closed)
+print('file is closed or not =',f.closed)
